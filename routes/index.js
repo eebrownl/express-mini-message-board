bd3b1d9b94317649c1e5b2d90b23a2a4f2ae1d64
Erika Brownlee <eebrownl@gmail.com>
2023-02-23 11:40:14 -0700

    looped over messsages in index.ejs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Betty",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charlie",
+    added: new Date()
+  }
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Message Board', messages: messages });
 });
 
 module.exports = router;
