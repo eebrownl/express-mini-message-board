cc28b009723be2ae9fddda864774fc73a92e15bc
Erika Brownlee <eebrownl@gmail.com>
2023-02-23 12:56:20 -0700

    created form and set up post

--- routes/index.js
+++ routes/index.js
@@ -24,4 +24,13 @@ router.get('/new', function(req, res) {
   res.render('form')
 })
 
+router.post('/new', function(req, res) {
+  messages.push({
+    text: req.body.message,
+    user: req.body.user,
+    added: new Date()
+  })
+  res.redirect('/')
+})
+
 module.exports = router;
